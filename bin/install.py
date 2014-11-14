607a5dd425c862346cd356a1250b275a2ec79715
Jose Ortiz-Ubarri <jose.ortiz@hpcf.upr.edu>
2014-11-14 17:14:11 -0400

    Changes to the install assuming that HOME is the default directory and some other small changes

--- bin/install.py
+++ bin/install.py
@@ -41,20 +41,32 @@ def confirmInput(msg):
 		
 		
 		
-def createconfigfile(password):
+def createconfigfile(DB_USER, DB_PASS, DB_NAME, DB_HOST):
+	HOME  = os.getenv("HOME")
+	print "\nNow enter the following information to create the configuration file\n" 
+    	graphpath="%s/graphs/" % HOME
 
-	print "Now enter the following information to create the configuration file\n" 
-    	graphpath=confirmInput("Enter the complete graphs path for TOA, suggested: /home/<username>/public_html/graphs (directory must exist, create after instalation if necessary): ")
+	#confirmInput("Enter the complete graphs path for TOA, suggested: /home/<username>/public_html/graphs (directory must exist, create after instalation if necessary): ")
 
-	flowpath=confirmInput("Enter path where the Flow files will be located (directory must exist, create after instalation if necessary): ")
+	flowpath=confirmInput("Enter path where the Flow files are stored: ")
 
-	logspath=confirmInput("Enter the  complete path for TOA logs to be saved(directory must exist, create after instalation if necessary): ")
-	
-	crontime=confirmInput("Enter the new crontime (in unix time) for TOA's script executions (suggested 300): ")
-	oldesttime=confirmInput("Enter for how many years will Toa keep data in the database before deleting it, min 1 year  max 5 years (only int): ")
-	oldesttime=int(float(oldesttime)) #round down just incase
+	#logspath=confirmInput("Enter the  complete path for TOA logs to be saved(directory must exist, create after instalation if necessary): ")
+	logspath="/tmp"	
+	crontime=300
 
-	toapath=confirmInput("Enter the path where the toa directory is located (include the toa directory in the path): ")
+	oldesttime = 0
+	while oldesttime < 1:
+		# confirmInput("Enter the new crontime (in seconds) for TOA's script executions (suggested 300, ): ")
+		oldesttime=confirmInput("Enter for how many years will Toa keep data in the database before deleting it\nmin 1 year,  max 5 years (only int): ")
+		try:
+			oldesttime=int(float(oldesttime)) #round down just incase
+		except:
+			oldesttime = 0
+			print "Please insert only integer values"
+
+	toapath=HOME
+
+	#confirmInput("Enter the path where the toa directory is located (include the toa directory in the path): ")
 
 	configfile="""
 <!-- Warning do not leave spaces in between tags and the data, it will count as part of the data string -->
@@ -98,95 +110,97 @@ def createconfigfile(password):
 	return flowpath,graphpath,crontime,toapath
 
 
+def createDB():
+	DB_USER = confirmInput("Enter MySQL User to be use with Toa: ")
+	DB_PASS=confirmInput("Enter the password: ")
+	DB_NAME=confirmInput("Enter the database to be used: ")
+	DB_HOST='localhost'
+
+
+	print "Testing connection to database using toadb..."
+	try:
+		db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, host=DB_HOST,db=DB_NAME)
+		c = db.cursor()
+
+		print "Succesfully Connected"
+	except MySQLdb.Error, e:
+
+	   	print "Error %d: %s" % (e.args[0],e.args[1])
+    		if e.args[0]==1045:
+			print "Make sure you provided the correct password,database and user credentials"
+			print "Exiting installation with errors"
+    		exit(1)
+
+	print "Loading Database Schema..."
+	if os.path.exists('../db/flowsschema.sql'):
+		#os.system('mysql -u toadb -p%s -h %s Toa < db/flowsschema.sql'%(toapass,DB_HOST))
+		try:
+			file=open('../db/flowsschema.sql','r')
+			lines=(file.read()).split(";")
+			for query in lines:
+				c.execute(query)
+		except MySQLdb.Error,e:
+    			print "Error %d: %s" % (e.args[0],e.args[1])
+			print "ERROR: database could not be loaded from 'db/flowsschema.sql', exiting ..."
+			c.close()
+			exit(1)
+
+		print "Database loaded"
+	else:
+		print "Error: Database dump 'flowsschema.sql' not found, please place it in this directory"
+		exit(1)
+	
+	c.close()
+	return DB_USER, DB_PASS, DB_NAME, DB_HOST 
+
+def createUser():
+
+	print ("""Creating user for the web interface.""")
+
+	email=raw_input("Enter your email to be used as user account: ")
+	while re.match("([a-zA-Z0-9]|\.|_|-|\+|$)+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$",email)==None:
+        	print "Error: not a valid email adress"
+        	email=raw_input("Enter email: ")
+
+	print "Insert password for user %s" % email
+	userpass=getpasswd()
+	user=UserModel()
+	print DB_NAME, DB_USER, DB_PASS
+	if user.connect(DB_NAME,DB_USER, DB_PASS,flowpath,graphpath,crontime):
+		print user.Create('Admin',"55555555555",userpass,email,1)
+	else:
+        	print "ERROR: Database Connection Error\n"
+        	print "Exiting installation with errors"
+        	c.close()
+        	exit(1)
+
 ################### MAIN ################
 
 print """This installer will guide you through the installation of TOA"""
 print """Please follow the instructions carefully"""
-print 
+print
 
-DB_USER=confirmInput("Enter MySQL User to be use with Toa: ")
-DB_PASS=confirmInput("Enter the password: ")
-DB_NAME=confirmInput("Enter the database to be used\n")
-DB_HOST='localhost'
+DB_USER="toa_in"
+DB_PASS="toa_in"
+DB_NAME="toa_in"
+DB_HOST="localhost"
 
-print "Testing connection to database using toadb..."
-try:
-	db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, host=DB_HOST,db=DB_NAME)
-
-	c = db.cursor()
-	print "Succesfully Connected"
-except MySQLdb.Error, e:
-
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    if e.args[0]==1045:
-	print "Make sure you provided the correct password,database and user credentials"
-	print "Exiting installation with errors"
-    exit(1)
-
-print "Loading Database Schema..."
-if os.path.exists('../db/flowsschema.sql'):
-	#os.system('mysql -u toadb -p%s -h %s Toa < db/flowsschema.sql'%(toapass,DB_HOST))
-	try:
-		file=open('../db/flowsschema.sql','r')
-		lines=(file.read()).split(";")
-		for query in lines:
-			c.execute(query)
-	except MySQLdb.Error,e:
-    		print "Error %d: %s" % (e.args[0],e.args[1])
-		print "ERROR: database could not be loaded from 'db/flowsschema.sql', exiting ..."
-		c.close()
-		exit(1)
-
-	print "Database loaded"
-else:
-	print "Error: Database dump 'flowsschema.sql' not found, please place it in this directory"
-	exit(1)
-
-
+#DB_USER, DB_PASS, DB_NAME, DB_HOST = createDB()
 print "Success"
-print "Creating the configuration file"
-flowpath,graphpath,crontime, toapath=createconfigfile(DB_PASS)
 
-print "..Done, a file named config.xml was created in the ../etc directory. If desired the file can also be placed on /etc or ~/etc "
+print "Creating the configuration file"
+flowpath,graphpath,crontime, toapath=createconfigfile(DB_USER, DB_PASS, DB_NAME, DB_HOST)
+
+print "..Done, a file named config.xml was created in directory ../etc/"
+print
 
 print "Generating crontab commands"
 createcrontab(flowpath,toapath)
 
-print "..Done, a file named crontab  was created in the ../etc directory. Please copy its contents to the crontab (remember to update crontab if the path it uses change) "
+print "..Done, a file named crontab  was created in the ../etc directory."
+print "Please copy its contents to the crontab (remember to update crontab if any path changes)\n"
 
-print ("""Creating admin for the web interface.""")
-print "Insert password for admin 'toa'"
-
-userpass=getpasswd()
-phone=raw_input("Enter phone number(only digits)\n")
-while re.match("[0-9]{7,20}$",phone)==None :
-	print "ERROR: not a valid phone number (use only digits)"
-	phone=raw_input("Enter phone number(only digits)\n")
-
-email=raw_input("Enter email to be used with admin account. Emails are used as usernames in Toa.\n")
-while re.match("([a-zA-Z0-9]|\.|_|-|\+|$)+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$",email)==None:
-	print "Error: not a valid email adress"
-	email=raw_input("Enter email\n")
-
-
-user=UserModel()
-
-print DB_USER, DB_PASS
-
-if user.connect(DB_NAME,DB_USER, DB_PASS,flowpath,graphpath,crontime):
-	
-
-	user.Create('toa',phone,userpass,email,1)
-
-
-else:
-	print "ERROR: Database Connection Error\n"
-	print "Make sure the user 'toa' doesn't exist already"
-	print "Exiting installation with errors"
-	c.close()
-	exit(1)
+createUser()
 
 print("Done, you are now able to log in as admin using the toa user")
 
-
-c.close()
